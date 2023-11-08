4c3a7f9db9431f1271292cbbf9889869166d5516
sauce <s4uc3_sus@protonmail.Com>
2023-11-08 14:16:12 +0100

    init commit

---
+++ install_home.sh
@@ -0,0 +1,18 @@
+#1)copie les fichier du github
+#2) installe i3, urxvt,polybar,feh
+#3) copie config dans .config/i3/config
+#4) copie config.ini dans /etc/polybar/config.ini
+#5) copie les wallpaper dans .wallpaper
+#6) change le nom du répertoire utilisateur dans les deux dernières commandes
+#7) copie .Xressources dans le home
+#8) xrdb merge
+
+git clone aaaaaaaaaaaaaa
+apt install -y i3 rxvt-unicode polybar feh
+cp ./config ~/.config/i3/config
+cp ./config.ini /etc/polybar/config.ini
+cp wallpaper* ~/.wallpaper/
+sed -i "s/s4uc3/$USER/g" ~/.config/i3/config
+cp .Xresources ~/.Xresources
+cp ./fonts /usr/local/share/example
+xrdb ~/.Xressources
